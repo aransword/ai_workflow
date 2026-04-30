9e7a0d81f3e301170679a8c21d18e1efbb720727
김동욱 <140793632+aransword@users.noreply.github.com>
2026-04-30 15:26:41 +0900

    (#5) add multiply (#6)

--- src/main/java/com/example/service/CalculatorService.java
+++ src/main/java/com/example/service/CalculatorService.java
@@ -30,4 +30,16 @@ public class CalculatorService {
         log.info("Performing subtraction: {} - {}", num1, num2);
         return num1 - num2;
     }
+
+    /**
+     * Performs multiplication of two integers.
+     *
+     * @param num1 First number
+     * @param num2 Second number
+     * @return Result of num1 * num2
+     */
+    public int multiply(int num1, int num2) {
+        log.info("Performing multiplication: {} * {}", num1, num2);
+        return num1 * num2;
+    }
 }
